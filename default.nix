71085f895a72a072adb4b9f84787a1bf5988fd05
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-05-14 19:30:17 +0200

    Add nix shell file

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
-  name = "locate-pointer";
+  name = "st";
   nativeBuildInputs = [ pkgconfig ncurses ];
   buildInputs = with xorg; [ libX11 libXft ];
 }
